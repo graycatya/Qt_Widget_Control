945903d926821b6e6bbeb4f14d102f5803dd51ec
cat <31work@first.com>
2019-11-27 17:43:21 +0800

    new control

---
+++ ControlTools/IpLabel/iplabeledit.cpp
@@ -0,0 +1,6 @@
+#include "iplabeledit.h"
+
+IpLabelEdit::IpLabelEdit()
+{
+
+}
---
+++ ControlTools/IpLabel/iplabeledit.h
@@ -0,0 +1,12 @@
+#ifndef IPLABELEDIT_H
+#define IPLABELEDIT_H
+
+#include <QWidget>
+
+class IpLabelEdit
+{
+public:
+    IpLabelEdit();
+};
+
+#endif // IPLABELEDIT_H
